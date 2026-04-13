beb64616e974ed48735fcfa4efa5af608a8f8839
Jennifer Galas <jennifer.galas@scranton.edu>
2026-04-13 12:19:20 -0400

    Work on scss

--- _plugins/pagefind.rb
+++ _plugins/pagefind.rb
@@ -1,7 +1,9 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-  puts "=== Running Pagefind ==="
-
-  success = system("_bin/pagefind --site #{site.dest}")
-
-  puts "=== Pagefind #{success ? 'SUCCESS' : 'FAILED'} ==="
+  if Jekyll.env == "production"
+    puts "=== Running Pagefind ==="
+    success = system("_bin/pagefind --site #{site.dest}")
+    puts "=== Pagefind #{success ? 'SUCCESS' : 'FAILED'} ==="
+  else
+    puts "=== Skipping Pagefind in development environment ==="
+  end
 end
